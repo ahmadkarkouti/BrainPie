14d91583de317d25ecf71ed0a7acc031c756724c
FranckNdame <franck.mpouli@yahoo.com>
2019-01-26 10:27:48 +0000

    add cellids

---
+++ BrainPie/CellsID.swift
@@ -0,0 +1,14 @@
+//
+//  CellsID.swift
+//  BrainPie
+//
+//  Created by Franck-Stephane Ndame Mpouli on 26/01/2019.
+//  Copyright © 2019 Ahmad Karkouti. All rights reserved.
+//
+
+import Foundation
+
+let languageCellId = "languageCell"
+let courseCell = "courseCell"
+let continueCell = "ContinueCell"
+let languageHeaderCell = "languageHCell"
